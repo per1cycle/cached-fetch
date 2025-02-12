8ed4f9719430aed09834df8f9030ab2e7efc7c27
Huan Zhou <me@per1cycle.org>
2025-02-13 00:34:45 +0800

    update comment

--- cf.c
+++ cf.c
@@ -1,6 +1,13 @@
 // Only file of this project.
 #include <stdio.h>
 
+// options
+// -i <input-file-name> 	one url per list.
+// -a <automatic retry> 	default "on"
+// -c <in memory cached> 	default "on"
+
+// reference: https://github.com/freebsd/freebsd-src/blob/main/usr.bin/fetch/fetch.c
+
 int main()
 {
 
